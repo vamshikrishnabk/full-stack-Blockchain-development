7c1588d0897d34e39ebaa186af948a9daa55f365
vamshi krishna <vamshikrishna.b@engro.io>
2022-08-18 22:43:37 +0530

    kept === operator inplace of ==

--- assignment-1/leap_year.js
+++ assignment-1/leap_year.js
@@ -10,7 +10,7 @@ if (year % 100 === 0)
         console.log(`${year} is not a leap year`);
 
 else
-    if (year % 4 == 0)
+    if (year % 4 === 0)
         console.log(`${year} is leap year`);
     else
         console.log(`${year} is not a leap year`);
